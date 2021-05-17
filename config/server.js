f6a5fb0d51a6dddd1ddca82bf07ef51a9c8c86ad
Santiago Marcano <marcanomedia941@gmail.com>
2021-05-17 19:24:13 +0200

    redirect

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: '/api',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'efc30a968c7bf08530b4203d4e2c8f32'),
