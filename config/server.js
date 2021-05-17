6536efc1e55df4ec004cff3ebc1db8932a711a29
Santiago Marcano <marcanomedia941@gmail.com>
2021-05-17 20:52:07 +0200

    URLS

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://165.22.86.88/api/",
+  url: "/api/",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "efc30a968c7bf08530b4203d4e2c8f32"),
     },
-    url: "http://165.22.86.88/api/admin/",
+    url: "/admin/",
   },
 });
